9704edb8f66a0aee2a933772de7bd2ac377e9729
Pranav Sodani <sodanipranav2004@gmail.com>
2025-03-25 15:01:56 +0530

    basic counter

--- flutter_application_1/lib/mainFile.dart
+++ flutter_application_1/lib/mainFile.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 
-class mainFile extends StatelessWidget {
+class mainFile extends StatefulWidget {
   const mainFile({super.key});
+  @override
+  _mainFileState createState() => _mainFileState();
+}
+
+class _mainFileState extends State<mainFile> {
+  int _count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('My Home Page')),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(padding: EdgeInsets.all(10)),
-            Text('Hello World'),
-            Padding(padding: EdgeInsets.all(20)),
+            const Padding(padding: EdgeInsets.all(10)),
+            Text('You have pressed the button $_count times.'),
+            const Padding(padding: EdgeInsets.all(20)),
             ElevatedButton(
               onPressed: () {
-                print('Click!!!');
+                setState(() {
+                  _count += 1;
+                });
               },
-              child: Text('Button'),
+              child: const Text('+'),
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _count = 0;
+                });
+              },
+              child: const Text('Reset'),
             ),
           ],
         ),
